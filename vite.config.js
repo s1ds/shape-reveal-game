a00f9ada14d4bcddbf2afb753482eca80ae0f94b
Sidht <sid.mailbox@gmail.com>
2026-05-17 19:40:37 +0530

    Allow Coolify preview host

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['kcgq0cr42r42j87gv7nebnio.82.112.238.62.sslip.io'],
+  },
 })
